8ac848b350746b0a11700f68035205e1a8c43ec3
Дмитрий Силачёв <dsilachev@MacBook-Air-Dmitrij.local>
2025-05-07 14:40:20 +0300

    Fixed for flake8 and PEP8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -57,9 +57,9 @@ def post_detail(request, id):
 
 
 def category_posts(request, category_slug):
-  filtered_posts = [post for post in posts if post['category'] == category_slug]
-  return render(
-    request,
-    'blog/category.html',
-    {'category_slug': category_slug, 'posts': filtered_posts},
-  )
+    filtered_posts = [post for post in posts if post['category'] == category_slug]
+    return render(
+        request,
+        'blog/category.html',
+        {'category_slug': category_slug, 'posts': filtered_posts},
+    )
